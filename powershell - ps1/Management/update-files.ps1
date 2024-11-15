abfdc63704757cf8c4b709b51bbfcd2a20c05149
MarcAleVargas <marcosalevargas@icloud.com>
2024-11-15 09:48:01 -0400

    management powershell

---
+++ powershell - ps1/Management/update-files.ps1
@@ -0,0 +1,40 @@
+"""
+####################################################################################################
+This script helps you to keep app files up to date on destination removing manual drag and drop
+####################################################################################################
+"""
+function global:update-files{
+    param(
+        $Source,
+        $Folder,
+        $Destination
+    )
+    $username = $Env:USERNAME
+    $updates = ls -Path $Source
+    $main = $Folder
+    $result = @()
+    $Destination | ForEach-Object {
+        $destination = "$main\$($_)"
+        $file = (ls -Path "$main\$($_)")
+        $file | ForEach-Object {
+            foreach($update in $updates){
+                if($update.Name -eq $_.Name -and $update.LastWriteTime -ne $_.LastWriteTime){
+                    Copy-Item -Path "$source\$($update.Name)" -Destination "$destination\$($_)"
+                    $result+=[PsCustomObject]@{
+                        Status = "Updated"
+                        File = "$($_)"
+                        Location = "$destination"
+                    }
+                }elseif ($update.Name -eq $_.Name -and $update.LastWriteTime -eq $_.LastWriteTime) {
+                    $result+=[PsCustomObject]@{
+                        Status = "Up-To-Date"
+                        File = "$($_)"
+                        Location = "$destination"
+                    }
+                }
+            }
+        }
+    }
+    return $result
+}
+update-files -Source "Principal Location Here" -Folder "Destination General Folder" -Destination @("SubFolder to update files")
