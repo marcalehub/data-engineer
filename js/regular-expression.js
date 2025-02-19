b306b17e2b2c8c7c9c90b2940a1858cf10588431
MarcAleVargas <marcosalevargas@icloud.com>
2025-02-19 17:06:29 -0400

    doc updating

--- js/regular-expression.js
+++ js/regular-expression.js
@@ -1,6 +1,7 @@
 // regular expressions
 // Regular expressions are patterns used to match character combinations in strings. In JavaScript, regular expressions are also objects. These patterns are used with the exec and test methods of RegExp, and with the match, replace, search, and split methods of String. This chapter describes JavaScript regular expressions.
-var regex = "[a-zA-Z]";
+var regex = /[a-zA-Z]/g;
 var str = "Hello World";
 var res = str.match(regex);
-console.log(res)
+console.log(res); //Unique match
+//console.log(res.join(', ')); Multiple match join by commas
